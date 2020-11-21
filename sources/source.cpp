081d351d805f437beb4305af82ed282a9021e14a
volpertinger <merzlovnik@mail.ru>
2020-11-21 22:21:47 +0300

    final_01

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,35 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Merzlov Nikolay merzlovnik@mail.ru
 
-#include <header.hpp>
+#include "header.hpp"
+
+// std::thread::hardware_concurrency() == 8;
+
+int main(int const arguments_count, char const* arguments[]) {
+  srand(time(0));
+
+  if (arguments_count > 2) {
+    std::cerr << "To much arguments" << std::endl;
+    return 1;
+  }
+
+  size_t threads_count;
+
+  if (arguments_count == 2) {
+    threads_count = size_t(arguments[1]);
+    BOOST_LOG_TRIVIAL(info) << arguments[1] << " traces";
+  }
+
+  if (arguments_count == 1) {
+    threads_count = std::thread::hardware_concurrency();
+    BOOST_LOG_TRIVIAL(info) << threads_count << " threads";
+  }
+
+  std::vector<std::thread> threads;
+
+  for (size_t i = 0; i < threads_count; ++i)
+    threads.push_back(std::thread(SHA256::find));
+
+  for (size_t i = 0; i < threads_count; ++i) threads[i].join();
+
+  return 0;
+}
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,35 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Merzlov Nikolay merzlovnik@mail.ru
 
 #include <gtest/gtest.h>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+#include "header.hpp"
+
+TEST(Hash, is_right_hash) {
+  SHA256 ss;
+  ss.hash = "hfskzdlfhdlsagfxckzuyhckcy,jkydfszxv0000";
+  EXPECT_EQ(true, ss.is_right_hash());
+  ss.hash = "hfskzdlfhdlsagfxckzuyhckcy,jkydfszxv00000";
+  EXPECT_EQ(true, ss.is_right_hash());
+  ss.hash = "0000";
+  EXPECT_EQ(true, ss.is_right_hash());
+  ss.hash = "hfskzdlfhdlsagfxckzuyhckcy,jkydfszxv00001";
+  EXPECT_EQ(false, ss.is_right_hash());
+  ss.hash = "hfskzdlfhdlsagfxckzuyhckcy,jkydfszxv100001";
+  EXPECT_EQ(false, ss.is_right_hash());
+  ss.hash = "hfskzdlfhdlsagfxckzuyhckcy,jkydfszxv00010";
+  EXPECT_EQ(false, ss.is_right_hash());
+}
+
+TEST(Hash, hash_gen) {
+  SHA256 ss;
+  ss.hash_gen();
+  EXPECT_EQ(ss.hash, picosha2::hash256_hex_string(ss.data));
+  ss.hash_gen();
+  EXPECT_EQ(ss.hash, picosha2::hash256_hex_string(ss.data));
+  ss.hash_gen();
+  EXPECT_EQ(ss.hash, picosha2::hash256_hex_string(ss.data));
+  ss.hash_gen();
+  EXPECT_EQ(ss.hash, picosha2::hash256_hex_string(ss.data));
+  ss.hash_gen();
+  EXPECT_EQ(ss.hash, picosha2::hash256_hex_string(ss.data));
 }
